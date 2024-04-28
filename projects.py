ab86c821283ad11b61f2bfb88294b40a6215222d
Kilo <kayjaypr@gmail.com>
2024-04-28 00:13:56 -0700

    Add projects list and templates

---
+++ projects.py
@@ -0,0 +1,10 @@
+projects = [
+    {
+      "image": "/static/images/user_placeholder.png",
+      "name": "order system",
+      "techs": ["Object Oriented Programming", "classes"],
+      "description": "this is a description",
+      "live": "#",
+      "repo": "https://github.com/Kilosince/buildfactory"
+    }
+]
